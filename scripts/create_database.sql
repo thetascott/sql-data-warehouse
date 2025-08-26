52f4477c12b50d9a41bad9e352031ba5f89ba84f
thetascott <sslivnik@gmail.com>
2025-08-25 23:21:33 -0500

    Create create_database.sql

---
+++ scripts/create_database.sql
@@ -0,0 +1,16 @@
+--
+======================================================================================================================
+Create Database
+======================================================================================================================
+Script Purpose:
+This script creates a new database named 'DataWarehouse'. If the database exists, it is dropped and recreated.
+
+WARNING:
+This script will drop the entire 'DataWarehouse' database if it exists. All data in the database will be permanently deleted. Proceed with caution!!!
+--
+
+-- Drop database if it exists
+DROP DATABASE IF EXISTS "DataWarehouse";
+
+-- Create database
+CREATE DATABASE "DataWarehouse";
