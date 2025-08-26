7e9d86bf33f58013b5081d644530334c1a106bb7
thetascott <sslivnik@gmail.com>
2025-08-25 23:23:14 -0500

    Create create_database_schemas.sql

---
+++ scripts/create_database_schemas.sql
@@ -0,0 +1,12 @@
+--
+======================================================================================================================
+Create Database
+======================================================================================================================
+Script Purpose:
+This script creates three new schemas named 'bronze', 'silver', and 'gold'.
+--
+
+-- Create schemas
+CREATE SCHEMA bronze;
+CREATE SCHEMA silver;
+CREATE SCHEMA gold;
